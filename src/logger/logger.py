35b426c77dbb047d5f8875e71eb40a871acaa50c
Travis <tclark2@rocketsoftware.com>
2025-11-03 14:44:13 -0600

    Logger refactor

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,3 +1,8 @@
+"""
+Logger module that provides a wrapper around the loguru logger.
+Provides methods for logging security and event-related messages.
+Configuration is read from config.ini.
+"""
 import os
 import sys
 import configparser
@@ -5,87 +10,101 @@ from typing import Any
 from loguru import logger as _logger
 from cryptography.fernet import Fernet
 
-
-encryption_key_path = os.path.join(os.path.dirname(__file__),
-                                   "../../env/log_key")
-
-# ensure directory exists before opening
-os.makedirs(os.path.dirname(encryption_key_path), exist_ok=True)
-
-if os.path.exists(encryption_key_path):
-    with open(encryption_key_path, 'rb') as file:
-        key = file.read()
-    if len(key) != 44:
-        key = Fernet.generate_key()
-        with open(encryption_key_path, 'wb') as file:
-            file.write(key)
-else:
-    key = Fernet.generate_key()
-    touch_dir = os.path.dirname(encryption_key_path)
-    if not os.path.exists(touch_dir):
-        os.makedirs(touch_dir)
-    with open(encryption_key_path, 'wb') as file:
-        file.write(key)
-
-cipher = Fernet(key)
-# Encryption key for security logs (in production, use a secure key management)
-
-config_file = configparser.ConfigParser()
-config_file.read('./config.ini')
-log_config = config_file['log']
-event_log_config = config_file['event_log']
-security_log_config = config_file['security_log']
-log_path = os.path.join(os.path.dirname(__file__), log_config['log_path'])
+CIPHER = None
+LOG_CONFIG = None
 
 
 def encrypt_message(message: str) -> bytes:
-    encrypted = cipher.encrypt(message.encode())
+    encrypted = CIPHER.encrypt(message.encode())
     return encrypted
 
 
 def encrypted_formatter(record):
-    formatted = log_config['format'].format(**record)
-    encrypted = cipher.encrypt(formatted.encode())
+    formatted = LOG_CONFIG['format'].format(**record)
+    encrypted = CIPHER.encrypt(formatted.encode())
     return encrypted.decode() + '\n'
 
 
-# Remove default stderr handler
-_logger.remove()
+def init_key():
+    encryption_key_path = os.path.join(os.path.dirname(__file__),
+                                       "../../env/log_key")
 
-retention = f"{log_config['retention_time']} {log_config['retention_unit']}"
-rotation = f"{log_config['rotation_size']} {log_config['rotation_unit']}"
+    # ensure directory exists before opening
+    os.makedirs(os.path.dirname(encryption_key_path), exist_ok=True)
 
-# Add security log
-security_log_file = os.path.join(log_path, security_log_config['file'])
-_logger.add(
-        security_log_file,
-        rotation=rotation,
-        retention=retention,
-        level=security_log_config['file_level'],
-        format=encrypted_formatter,
-        filter=lambda record, name="security": record["extra"]
-        .get("type") == name
-        )
+    if os.path.exists(encryption_key_path):
+        with open(encryption_key_path, 'rb') as file:
+            key = file.read()
+        if len(key) != 44:
+            key = Fernet.generate_key()
+            with open(encryption_key_path, 'wb') as file:
+                file.write(key)
+    else:
+        key = Fernet.generate_key()
+        touch_dir = os.path.dirname(encryption_key_path)
+        if not os.path.exists(touch_dir):
+            os.makedirs(touch_dir)
+        with open(encryption_key_path, 'wb') as file:
+            file.write(key)
 
-# Add event log
-event_log_file = os.path.join(log_path, event_log_config['file'])
-_logger.add(
-        event_log_file,
-        level=event_log_config['file_level'],
-        retention=retention,
-        rotation=rotation,
-        format=log_config['format'],
-        filter=lambda record, name=event_log_config['name']: record["extra"]
-        .get("type") == name
-        )
+    global CIPHER
+    CIPHER = Fernet(key)
+    # Encryption key for security logs (in production, use a secure key management)
 
-# Configure separate console (stdout) logging levels for security and event
-_logger.add(sys.stderr, level=event_log_config['console_level'],
-            filter=lambda record: record["extra"].get("type") == "security",
-            format=log_config['format'])
-_logger.add(sys.stderr, level=security_log_config['console_level'],
-            filter=lambda record: record["extra"].get("type") == "event",
-            format=log_config['format'])
+
+def init_logger():
+    """
+    Initializes the logger.
+    """
+    init_key()
+
+    config_file = configparser.ConfigParser()
+    config_file.read('./config.ini')
+    LOG_CONFIG = config_file['log']
+    event_LOG_CONFIG = config_file['event_log']
+    security_LOG_CONFIG = config_file['security_log']
+    log_path = os.path.join(os.path.dirname(__file__), LOG_CONFIG['log_path'])
+
+
+    # Remove default stderr handler
+    _logger.remove()
+
+    retention = f"{LOG_CONFIG['retention_time']} {LOG_CONFIG['retention_unit']}"
+    rotation = f"{LOG_CONFIG['rotation_size']} {LOG_CONFIG['rotation_unit']}"
+
+    # Add security log
+    security_log_file = os.path.join(log_path, security_LOG_CONFIG['file'])
+    _logger.add(
+            security_log_file,
+            rotation=rotation,
+            retention=retention,
+            level=security_LOG_CONFIG['file_level'],
+            format=encrypted_formatter,
+            filter=lambda record, name="security": record["extra"]
+            .get("type") == name
+            )
+
+    # Add event log
+    event_log_file = os.path.join(log_path, event_LOG_CONFIG['file'])
+    _logger.add(
+            event_log_file,
+            level=event_LOG_CONFIG['file_level'],
+            retention=retention,
+            rotation=rotation,
+            format=LOG_CONFIG['format'],
+            filter=lambda record, name=event_LOG_CONFIG['name']: record["extra"]
+            .get("type") == name
+            )
+
+    # Configure separate console (stdout) logging levels for security and event
+    _logger.add(sys.stderr, level=event_LOG_CONFIG['console_level'],
+                filter=lambda record: record["extra"].get("type") == "security",
+                format=LOG_CONFIG['format'])
+    _logger.add(sys.stderr, level=security_LOG_CONFIG['console_level'],
+                filter=lambda record: record["extra"].get("type") == "event",
+                format=LOG_CONFIG['format'])
+
+
 
 
 class Logger:
@@ -104,14 +123,20 @@ class Logger:
         """
         Logs security-related messages to security.log at the specified level.
         Messages are encrypted for security.
+        Args:
+            message (str): The message to log.
+            level (str, optional): The log level. Defaults to "info".
         """
-        getattr(self._logger.bind(type="security"), level)(message, *args,
-                                                           **kwargs)
+        getattr(self._logger.bind(type="security").opt(depth=1), level)(
+                message, *args, **kwargs)
 
     def event(self, message: str, level: str = "info", *args: Any,
               **kwargs: Any) -> None:
         """
         Logs event-related messages to event.log at the specified level.
+        Args:
+            message (str): The message to log.
+            level (str, optional): The log level. Defaults to "info".
         """
-        getattr(self._logger.bind(type="event"), level)(message,
-                                                        *args, **kwargs)
+        getattr(self._logger.bind(type="event").opt(depth=1), level)(
+                message, *args, **kwargs)
