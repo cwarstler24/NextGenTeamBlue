fd4b8a6ebb54d5542c5218388872b9a2b57d04ce
Travis <tclark2@rocketsoftware.com>
2025-11-05 15:12:00 -0600

    Refactored database controller
    Updated all function to take in a user title

--- src/api/authorize.py
+++ src/api/authorize.py
@@ -1,5 +1,16 @@
-# src/autorize.py
 from fastapi import HTTPException, Request
+from enum import Enum
+from src.database.authorize import Role
+from src.logger import logger
+
+class EmployeeTitle(Enum):
+    MANAGER = "manager"
+    AIDE = "aide"
+    DEVELOPER = "developer"
+    SALESAGENT = "sales agent"
+
+class ManagerTitle(Enum):
+    MANAGER = "manager"
 
 async def authorize_request(request: Request, decoded_payload: dict):
     """
@@ -11,10 +22,10 @@ async def authorize_request(request: Request, decoded_payload: dict):
     method = request.method
 
     # Managers get full access
-    if user_title == "manager":
+    if user_title in ManagerTitle.__members__:
         return {
             "authorized": True,
-            "role": "Manager",
+            "role": Role.MANAGER,
             "allowed_methods": ["GET", "POST", "PUT", "DELETE"],
             "action": f"{method} request allowed"
         }
@@ -25,7 +36,34 @@ async def authorize_request(request: Request, decoded_payload: dict):
 
     return {
         "authorized": True,
-        "role": "Employee",
+        "role": Role.EMPLOYEE,
         "allowed_methods": ["GET"],
         "action": f"{method} request allowed"
     }
+
+
+def get_db_role(user_title: str = "") -> Role:
+    """
+    Returns the database role for the given user title.
+    Args:
+        user_title (str): The user's title.
+
+    Returns:
+        Role: The database role for the user.
+    """
+    logger.event(f"user_title: {user_title}", level="trace")
+
+    user_title = user_title.lower()
+    role = Role.OTHER
+    if user_title == "manager":
+        role = Role.MANAGER
+    elif user_title == "aide":
+        role = Role.AIDE
+    elif user_title == "developer":
+        role = Role.DEVELOPER
+    elif user_title == "sales agent":
+        role = Role.SALESAGENT
+
+
+
+    return role
